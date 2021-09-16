82d8031432ab5b3f9aaf9de123acd8f9251c8d06
yeslol234 <72095827+yeslol234@users.noreply.github.com>
2021-09-15 18:19:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $('body').terminal({
+    // COMMANDS
     echo: function(text) {
         this.echo(text);
+    },
+    help: function() {
+        this.echo('Current Commands Are:\n echo: Simply display text\n help: Help for commands.')
     }
 }, {
     greetings: 'Terminal (Version 1.0)\nRepo: https://github.com/yeslol234/Terminal/tree/main'
