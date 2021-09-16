5b87f46e2712e1f3b65c4cc14ec2868065286a70
yeslol234 <72095827+yeslol234@users.noreply.github.com>
2021-09-15 18:12:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ $('body').terminal({
         this.echo(text);
     }
 }, {
-    greetings: 'Terminal (Version 1.0)\nRepo: https://github.com'
+    greetings: 'Terminal (Version 1.0)\nRepo: https://github.com/yeslol234/Terminal/tree/main'
 });
